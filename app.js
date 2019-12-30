4d03c2cb7952aafb9410e09a5c0fe4b2c31978a8
shun <1039849632@qq.com>
2019-12-30 17:14:42 +0800

    add pug

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const koa = require('koa')
 const app = new koa()
+const views = require('koa-views')
+const {resolve} = require('path')
+// 除了需要安装koa-views还需要安装pug
+app.use(views(resolve(__dirname,'./views'),{
+  extension: 'pug'
+}))
 app.use(async (ctx, next)=> {
-  ctx.body = 'hello koa2'
+  // ctx.render会把pug解析成str返回
+  await ctx.render('index',{
+    you: 'Luke',
+    me: 'SHUNdai'
+  })
 })
 app.listen(3000)
---
+++ template/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  normal: require('./htm'),
+  ejs: require('ejs')
+}
